8d33403d5caa37ec44cfa3918c1a92c7bd3de891
Ashwin Ramesh <ashwinr64@gmail.com>
2016-03-09 08:31:39 +0530

    jalebi: Kang GPS HAL from Kipper
    Change-Id: If1e7adca3e1cad01df1f4e9379f8ba7f85afbcc0

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -38,10 +38,14 @@ LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_
 
+ifeq ($(QCPATH),)
+LOCAL_CFLAGS += -DOSS_BUILD
+endif
+
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
-    $(call project-path-for,qcom-gps)/loc_api/libloc_api_50001 \
+    $(LOCAL_PATH) \
     $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
@@ -90,6 +94,10 @@ LOCAL_CFLAGS += \
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
 LOCAL_CFLAGS += -DTARGET_USES_QCOM_BSP
+endif
+
+ifeq ($(QCPATH),)
+LOCAL_CFLAGS += -DOSS_BUILD
 endif
 
 ## Includes
