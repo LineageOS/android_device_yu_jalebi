f01a3da4f207d8a91d645e0b1f85973bec246090
FireLord1 <firelord.xda@gmail.com>
2016-08-27 11:06:42 +0530

    jalebi: ship gello
    Change-Id: Iffc39760324879bf52fd371ff3481b6d61b0c4a5

--- device.mk
+++ device.mk
@@ -139,5 +139,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwcnss_qmi
 
+# Browser
+PRODUCT_PACKAGES += \
+    Gello
+
 # Inherit the rest from msm8916-common
 $(call inherit-product, device/cyanogen/msm8916-common/msm8916.mk)
